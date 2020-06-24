5d5da15f7fb2a352171e8e97d5fd9e2c84692909
Andrii Stepanets <ovrass@gmail.com>
2020-06-24 17:30:54 +0300

    Added componentDidCatch to single components, details in peoplepage component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
+import PeoplePage from '../people-page';
 import ErrorIndicator from '../error-indicator';
 
 import './app.css';
@@ -12,14 +11,7 @@ export default class App extends Component {
 
   state = {
     showRandomPlanet: true,
-    selectedPerson: 5,
     hasError: false
-  };
-
-  onPersonSelected = (id) => {
-    this.setState({
-      selectedPerson: id
-    });
   };
 
   componentDidCatch(){
@@ -35,14 +27,9 @@ export default class App extends Component {
         <Header />
         <RandomPlanet />
 
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.onPersonSelected}/>
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson}/>
-          </div>
-        </div>
+        <PeoplePage />
+        <PeoplePage />
+        <PeoplePage />
       </div>
     );
   }
