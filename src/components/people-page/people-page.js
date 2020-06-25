e28407510f8859247c39e940b328d3cb75f2b6ed
Andrii Stepanets <ovrass@gmail.com>
2020-06-25 20:15:07 +0300

    Added pattern render function to display details persons, planets and starships

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -25,7 +25,7 @@ export default class PeoplePage extends Component {
     onPersonSelected = (selectedPerson) => {
         this.setState({ selectedPerson });
     }
-    
+
     render(){
         if (this.state.hasError) {
             return <ErrorIndicator />
@@ -35,7 +35,10 @@ export default class PeoplePage extends Component {
                 <div className="col-md-6">
                     <ItemList 
                         onItemSelected={this.onPersonSelected} 
-                        getData={this.swapiService.getAllPeople}/>
+                        getData={this.swapiService.getAllPeople}
+                        renderItem={( { name, gender, birthYear} ) => (
+                            `${name} (${gender}), ${birthYear}`
+                            )}/>
                 </div>
                 <div className="col-md-6">
                     <PersonDetails personId={this.state.selectedPerson} />
