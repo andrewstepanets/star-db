bc0d3ced368a72939f17c1759ca748418ca8b758
Andrii Stepanets <ovrass@gmail.com>
2020-06-30 18:59:07 +0300

    Create high order component with swapi service to simplified details component, divided details component to three different components

--- src/components/sw-components/index.js
+++ src/components/sw-components/index.js
@@ -1,9 +1,7 @@
+import PersonDetails from './person-details';
+import PlanetDetails from './planet-details';
+import StarshipDetails from './starship-details';
 
-import {
-    PersonDetails,
-    PlanetDetails,
-    StarshipDetails
-} from './details';
 
 import {
     PersonList,
